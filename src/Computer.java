6b52b7084f7e3a63a6c0127a70eef21efff7ee03
bendlas <hhochleitner@gmail.com>
2010-12-09 13:17:13 +0100

    DeviceHolder and Computer

---
+++ src/Computer.java
@@ -0,0 +1,10 @@
+import java.util.Collection;
+
+
+public class Computer implements DeviceHolder {
+	@Override
+	public Collection<Device> volumes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ src/DeviceHolder.java
@@ -0,0 +1,5 @@
+import java.util.Collection;
+
+public interface DeviceHolder {
+	Collection<Device> volumes();
+}
