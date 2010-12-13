376f4ef88324f9cc735cf9f571267782d4c5da9f
noce <e0827181@student.tuwien.ac.at>
2010-12-13 13:23:35 +0100

    slot & subklassen zusicherungen

--- src/CF1Slot.java
+++ src/CF1Slot.java
@@ -1,11 +1,16 @@
 
 public class CF1Slot extends Slot {
 
+	
 	@Override
 	public String getName() {
 		return "CF I Slot";
 	}
 
+	//pre: is called if Slot is empty
+	//post: returns whether insert worked 
+	//post: checks if medium fits into slot, and inserts if so
+	//this method is overridden to call a appropriate Visitor Method in the Medium (emulated Multimethod)
 	@Override
 	protected boolean doInsert(Medium medium) {
 		return medium.insertToCF1Slot(this);
