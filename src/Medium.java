393f1d6fb3125ad676c0e73905c8afb5da22ffdd
babz <babz@babz-laptop.(none)>
2010-12-13 13:16:19 +0100

    zusicherungen

--- src/Medium.java
+++ src/Medium.java
@@ -1,7 +1,10 @@
 
 abstract public class Medium implements Device {
-	protected Slot holder;
+	private Slot holder;
 	
+	/*
+	 * post: ejects medium from slot (if not empty) and marks slot empty
+	 */
 	protected void eject() {
 		while (holder != null) {
 			holder.eject();
@@ -17,6 +20,10 @@ abstract public class Medium implements Device {
 	/**
 	 * Call this to actually insert the Medium
 	 */
+	/*
+	 * pre: inserts medium into slot only if slot is empty
+	 * post: return true if insert successful
+	 */
 	protected boolean doInsert(Slot slot) {
 		while ( holder == null) {
 			holder = slot;
@@ -25,33 +32,92 @@ abstract public class Medium implements Device {
 		return false;
 	}
 	
+	/*
+	 * pre: inserts medium into slot only if slot is empty
+	 * post: inserts medium into USBSlot and return false;
+	 * 		 if insert is successful, Slot.insert returns true
+	 */
 	protected boolean insertToUSBSlot(USBSlot port) {
 		return false;
 	}
+	
+	/*
+	 * pre: inserts medium into slot only if slot is empty
+	 * post: inserts medium into BlueRaySlot and return false;
+	 * 		 if insert is successful, Slot.insert returns true
+	 */
 	protected boolean insertToBlueRaySlot(BlueRaySlot slot){
 		return false;
 	}
+	
+	/*
+	 * pre: inserts medium into slot only if slot is empty
+	 * post: inserts medium into CDSlot and return false;
+	 * 		 if insert is successful, Slot.insert returns true
+	 */
 	protected boolean insertToCDSlot(CDSlot slot){
 		return false;
 	}
+	
+	/*
+	 * pre: inserts medium into slot only if slot is empty
+	 * post: inserts medium into CF1Slot and return false;
+	 * 		 if insert is successful, Slot.insert returns true
+	 */
 	protected boolean insertToCF1Slot(CF1Slot slot){
 		return false;
 	}
+	
+	/*
+	 * pre: inserts medium into slot only if slot is empty
+	 * post: inserts medium into CF2Slot and return false;
+	 * 		 if insert is successful, Slot.insert returns true
+	 */
 	protected boolean insertToCF2Slot(CF2Slot slot){
 		return false;
 	}
+	
+	/*
+	 * pre: inserts medium into slot only if slot is empty
+	 * post: inserts medium into DVDSlot and return false;
+	 * 		 if insert is successful, Slot.insert returns true
+	 */
 	protected boolean insertToDVDSlot(DVDSlot slot){
 		return false;
 	}
+	
+	/*
+	 * pre: inserts medium into slot only if slot is empty
+	 * post: inserts medium into MemoryStickSlot and return false;
+	 * 		 if insert is successful, Slot.insert returns true
+	 */
 	protected boolean insertToMemoryStickSlot(MemoryStickSlot slot){
 		return false;
 	}
+	
+	/*
+	 * pre: inserts medium into slot only if slot is empty
+	 * post: inserts medium into MicroSDSlot and return false;
+	 * 		 if insert is successful, Slot.insert returns true
+	 */
 	protected boolean insertToMicroSDSlot(MicroSDSlot slot){
 		return false;
 	}
+	
+	/*
+	 * pre: inserts medium into slot only if slot is empty
+	 * post: inserts medium into MiniSDSlot and return false;
+	 * 		 if insert is successful, Slot.insert returns true
+	 */
 	protected boolean insertToMiniSDSlot(MiniSDSlot slot){
 		return false;
 	}
+	
+	/*
+	 * pre: inserts medium into slot only if slot is empty
+	 * post: inserts medium into SDSlot and return false;
+	 * 		 if insert is successful, Slot.insert returns true
+	 */
 	protected boolean insertToSDSlot(SDSlot slot){
 		return false;
 	}
